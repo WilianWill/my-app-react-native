94ee0f27e69b51f92a783e491b7d965bfc4ac313
WilianWill <165562448+WilianWill@users.noreply.github.com>
2024-12-02 19:43:47 -0300

    balance componet

---
+++ components/balance-component/balance.js
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Balance() {
+  const [saldo, setSaldo] = useState(1000);
+  const [despesas, setDespesas] = useState(386);
+
+  const saldoStyle = {
+    color: saldo > 0 ? "green" : "red",
+  };
+
+  const saldoTotal = `R$ ${saldo.toFixed(2)}`;
+  const despesaTotal = `R$ - ${despesas.toFixed(2)}`;
+
+  return (
+    <View style={style.balanceContainer}>
+      <View style={style.balanceContentDisponivel}>
+        <Text style={style.balanceTitle}>Saldo Disponível</Text>
+        <Text style={[style.balanceSaldo, saldoStyle]}>{saldoTotal}</Text>
+      </View>
+
+      <View style={style.balanceContentDespesas}>
+        <Text style={style.balanceTitle}>Gastos</Text>
+        <Text style={style.balanceGastos}>{despesaTotal}</Text>
+      </View>
+    </View>
+  );
+}
+
+const style = StyleSheet.create({
+  balanceContainer: {
+    backgroundColor: "#fff",
+    marginTop: -5,
+    height: 100,
+    borderRadius: 10,
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginInline: 10,
+    paddingInline: 14,
+  },
+
+  balanceContentDisponivel: {
+    alignItems: "flex-start",
+  },
+  balanceContentDespesas: {
+    alignItems: "flex-end",
+  },
+
+  balanceTitle: {
+    fontWeight: "bold",
+    fontSize: 18,
+    color: "gray",
+  },
+
+  balanceGastos: {
+    fontSize: 20,
+    color: "red",
+  },
+  balanceSaldo: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+});
