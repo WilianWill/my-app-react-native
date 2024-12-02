329f6fce254273891d54626963badb9c6b75b810
WilianWill <165562448+WilianWill@users.noreply.github.com>
2024-12-02 19:44:22 -0300

    remove some styles

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Header from "./components/header-component/header";
+import Balance from "./components/balance-component/balance";
+import Feature from "./components/feature-options/feature";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header />
+      <Balance />
+      <Feature />
     </View>
   );
 }
@@ -13,8 +17,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "whitesmoke",
   },
 });
